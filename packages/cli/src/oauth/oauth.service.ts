4a14f614c29191404a19f69454fb7539f5b13ad9
achille-policante <policanteachille@gmail.com>
2026-08-24 12:31:35 +0200

    Revert "refactor: remove scripts and dependencies from package.json files across frontend workspaces"
    This reverts commit a0c7ca83e167ef0cb909db2bfa9597cb15b6b06d.

--- packages/cli/src/oauth/oauth.service.ts
+++ packages/cli/src/oauth/oauth.service.ts
@@ -36,7 +36,6 @@ import {
 	oAuthAuthorizationServerMetadataSchema,
 	dynamicClientRegistrationResponseSchema,
 } from '@/controllers/oauth/oauth2-dynamic-client-registration.schema';
-// @ts-ignore
 import pkceChallenge from 'pkce-challenge';
 import * as qs from 'querystring';
 import split from 'lodash/split';
