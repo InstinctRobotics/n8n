e4c370e8de86a31f6996a8937f33621ce3d4a124
achille-policante <policanteachille@gmail.com>
2026-09-16 12:37:02 +0200

    refactor: remove duplicate custom_nodes folder and update docker-compose volume paths

--- custom_nodes/gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-
-gulp.task('build:icons', function () {
-	return gulp.src(['nodes/**/*.{png,svg}', 'credentials/**/*.{png,svg}'], { base: '.' })
-		.pipe(gulp.dest('dist'));
-});
